b15358defe5c2f6cc9e1b755df6873c47dbcadee
Jules-Latapy <76938929+Jules-Latapy@users.noreply.github.com>
2023-06-06 11:12:22 +0200

    Update index.tsx

--- with-cypress-app/pages/index.tsx
+++ with-cypress-app/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Find in-depth information about Next.js fritures and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
