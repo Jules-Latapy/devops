1818d8599a79ad747ba2567e9928e29a32e62aaa
Jules-Latapy <76938929+Jules-Latapy@users.noreply.github.com>
2023-06-06 10:43:40 +0200

    Update index.tsx

--- with-cypress-app/pages/index.tsx
+++ with-cypress-app/pages/index.tsx
@@ -66,7 +66,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Fait avec{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
